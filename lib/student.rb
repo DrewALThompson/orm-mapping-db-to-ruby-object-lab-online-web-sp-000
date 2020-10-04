58b95e0a549225cfd65c5c4b63483473996bbd67
Drew Thompson <drewallynthompson@gmail.com>
2020-10-04 09:31:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -72,6 +72,10 @@ class Student
   end 
   
   def self.first_student_in_grade_10
+    sql = <<-SQL 
+      SELECT * FROM students WHERE grade = 10 Limit 1
+    SQL
+    DB[:conn].execute(sql).map {|row|self.new_from_db(row)}
   end
   
   def self.all_students_in_grade_X
