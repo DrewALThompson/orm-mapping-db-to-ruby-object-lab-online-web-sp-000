4505bf2e55705ecc994ddf936721720c626900cd
Drew Thompson <drewallynthompson@gmail.com>
2020-10-04 09:13:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,7 +60,7 @@ class Student
   end
   
   def self.students_below_12th_grade
-    sql = "SELECT COUNT(grade > 12) FROM students"
+    sql = "SELECT COUNT(grade >= 11) FROM students"
     DB[:conn].execute(sql)
   end
 end
