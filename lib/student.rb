77a719aca9f73724ddf29ee73f4283a6c577dd84
Drew Thompson <drewallynthompson@gmail.com>
2020-10-04 09:20:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,7 +62,7 @@ class Student
   def self.students_below_12th_grade
     sql = <<-SQL
       SELECT * FROM students
-      WHERE students.grade > 12
+      WHERE students.grade < 12
     SQL
     DB[:conn].execute(sql)
   end
